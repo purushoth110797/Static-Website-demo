f96545255c56e730722f570aa68c445857eeca9e
unknown <npurush11@gmail.com>
2023-10-21 17:29:34 +0530

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function scrollToMenu() {
+    const section = document.getElementById("explore-menu-section-id");
+    if (section) {
+        section.scrollIntoView({ behavior: "smooth" });
+    }
+}
+function scrollToPayment() {
+    const section = document.getElementById("delivery-and-payment-section-id");
+    if (section) {
+        section.scrollIntoView({ behavior: "smooth" });
+    }
+}
